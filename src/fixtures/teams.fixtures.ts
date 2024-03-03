5e2022933b8809199a98eaf4804583f13c515551
Trevor Pierce <trevthewebdev@gmail.com>
2024-03-03 12:47:22 -0600

    adds fixture data

---
+++ src/fixtures/teams.fixtures.ts
@@ -0,0 +1,13 @@
+export default [
+  {
+    id: 1,
+    name: "React",
+    description:
+      "Focuses on creating react-based libraries and teaching deep react topics",
+  },
+  {
+    id: 2,
+    name: "Javascript Mastery",
+    description: "Savants of javascript and software engineering",
+  },
+];
